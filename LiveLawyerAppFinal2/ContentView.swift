e583057fd03121f26eb0ccf75ecde8af23bc16f1
Jeslyn Guo <jguo@greenwichacademy.org>
2021-06-03 12:46:42 -0400

    Initial Commit

---
+++ LiveLawyerAppFinal2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiveLawyerAppFinal2
+//
+//  Created by Jeslyn Guo '22 on 6/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
